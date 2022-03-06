4db0250cf1e5ee9f3e22223b8798ada6164b097d
Zuoqiu-Yingyi <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2022-03-06 12:50:32 +0800

    feat: 使用快捷键 <kbd>Shift + Alt + D</kbd> 删除当前文档全部内容 | Use the shortcut keys <kbd>Shift + Alt + D</kbd> to delete the full content of the current documen.

---
+++ script/module/doc.js
@@ -0,0 +1,66 @@
+/* 复制加强 */
+
+import { config } from '/appearance/themes/Dark+/script/module/config.js';
+import { isKey } from '/appearance/themes/Dark+/script/utils/hotkey.js';
+import {
+    exportMdContent,
+    updateBlock,
+} from '/appearance/themes/Dark+/script/utils/api.js';
+
+async function docCopy() {
+    const background = document.querySelector('div.fn__flex-1.protyle:not(.fn__none)>div.protyle-content>div.protyle-background');
+    if (background) {
+        let id = background.getAttribute('data-node-id');
+        if (id) {
+            let data = await exportMdContent(id);
+            if (data) {
+                let content = data.content;
+                navigator.clipboard.writeText(content);
+            }
+        }
+    }
+}
+
+async function docDelete() {
+    const background = document.querySelector('div.fn__flex-1.protyle:not(.fn__none)>div.protyle-content>div.protyle-background');
+    if (background) {
+        let id = background.getAttribute('data-node-id');
+        if (id) {
+            await updateBlock(
+                id,
+                'markdown',
+                '',
+            );
+        }
+    }
+}
+
+(() => {
+    try {
+        if (config.doc.copy) {
+            let body = document.querySelector('body');
+
+            if (config.doc.copy.enable) {
+                // 复制当前文档全部内容至剪贴板
+                body.addEventListener('keydown', (e) => {
+                    // console.log(e);
+                    if (isKey(e, config.hotkeys.doc.copy)) {
+                        setTimeout(docCopy, 0);
+                    }
+                });
+            }
+
+            if (config.doc.delete.enable) {
+                // 删除当前文档全文
+                body.addEventListener('keydown', (e) => {
+                    if (isKey(e, config.hotkeys.doc.delete)) {
+                        console.log(e);
+                        setTimeout(docDelete, 0);
+                    }
+                });
+            }
+        }
+    } catch (err) {
+        console.error(err);
+    }
+})();
