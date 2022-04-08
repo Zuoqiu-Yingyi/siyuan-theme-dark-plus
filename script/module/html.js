188c29ef67521695fa483538d203eef7b3a92869
Zuoqiu-Yingyi <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2022-04-09 03:04:41 +0800

    feat: 添加在 HTML 块的 shadowRoot 中获取当前块的工具 `This(customID)` | Added tool `This(customID)` to get the current block in the shadowRoot of HTML block. .

--- script/module/html.js
+++ script/module/html.js
@@ -2,8 +2,27 @@
 
 /* 运行系统命令 */
 function runcmd(commands) {
-    if (window.confirm(commands)) {
+    if (window.confirm(commands) && require) {
         commands = `start powershell -c ${commands.replaceAll('\n', '; ')}pause`;
-        require('child_process').child_process.exec(commands, null);
+        require('child_process').exec(commands, null);
+    }
+}
+
+/**
+ * HTML 块中的脚本获取当前块
+ * @param {string} customID 内部定义的 ID
+ * @returns {string} id 当前 HTML 块 ID
+ * @returns {HTMLElement} block 当前 HTML 块
+ * @returns {HTMLElement} shadowRoot 当前 HTML 块 shadowRoot
+ */
+function This(customID) {
+    let protyle = document.querySelector(`protyle-html[data-content*="${customID}"]`);
+    if (protyle) {
+        let block = protyle.parentElement.parentElement;
+        return {
+            id: block.dataset.nodeId,
+            block: block,
+            shadowRoot: protyle.shadowRoot,
+        };
     }
 }
