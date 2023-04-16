af7745c364bebe27c8420680b03359a76694b536
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2023-04-16 18:32:04 +0800

    build: 更新 Monaco Editor 至 `v0.37.1` | Updated Monaco Editor to `v0.37.1`.

--- app/editor/vs/base/common/worker/simpleWorker.nls.es.js
+++ app/editor/vs/base/common/worker/simpleWorker.nls.es.js
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.34.0(4b8a47f3570a4a05ace9d00ae0df044b55befcd5)
+ * Version: 0.37.1(20a8d5a651d057aaed7875ad1c1f2ecf13c4e773)
  * Released under the MIT license
  * https://github.com/microsoft/vscode/blob/main/LICENSE.txt
  *-----------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls.es",{"vs/base/common/platform":["_"]});
